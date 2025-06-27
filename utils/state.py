dd3c1ad0c3034d01128f8d419b804aa0d5cbe8a6
MarkMiddendorf <markmiddendorf49@gmail.com>
2025-06-27 14:49:27 -0400

    Update state.py

--- utils/state.py
+++ utils/state.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 def init_session_state():
     defaults = {
-        #"display_toggle": "School Year / School Year"
+        if "display_toggle" not in st.session_state:
+        st.session_state["display_toggle"] = "School Year / School Year"
         # other shared keys...
     }
     for key, val in defaults.items():
