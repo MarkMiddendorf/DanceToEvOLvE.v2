b42aa51258bef950ad3b4780328a717c49cdb577
MarkMiddendorf <markmiddendorf49@gmail.com>
2025-06-27 14:49:50 -0400

    Update 2 Group_by.py

--- pages/2 Group_by.py
+++ pages/2 Group_by.py
@@ -4,7 +4,7 @@ from utils.state import init_session_state
 
 def main():
     apply_global_styles() 
-    #init_session_state()
+    init_session_state()
     #st.write("Session State Snapshot:", st.session_state)
     st.title("🧺 Group By")
     st.write("Group by logic here")
